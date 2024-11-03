f94a6209062ca15c64df2e273d64726c71ccc024
KennyLi6 <kennyli3219@gmail.com>
2024-11-02 17:17:57 -0700

    added a button that shows message to user after clicking

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 // todo
+
+import "./style.css";
+
+const APP_NAME = "Geocoin Carrier";
+const APP = document.querySelector<HTMLDivElement>("#app")!;
+
+document.title = APP_NAME;
+
+const BUTTON = document.createElement("button");
+const BUTTON_TEXT = "Click me!";
+BUTTON.innerHTML = BUTTON_TEXT;
+BUTTON.addEventListener("click", () => alert("you clicked the button!"));
+APP.append(BUTTON);
