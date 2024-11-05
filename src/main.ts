37f5d5122cfdf28e6c4d43edca04d434abed2e9d
KennyLi6 <kennyli3219@gmail.com>
2024-11-05 15:25:29 -0800

    added interfaces, events, etc that are listed as useful in the slides

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,54 @@
-// todo
+import leaflet from "leaflet";
 
 import "./style.css";
+
+// Work around bug in Leaflet (https://github.com/Leaflet/Leaflet/issues/4968)
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+delete (leaflet.Icon.Default.prototype as unknown as { _getIconUrl: unknown })
+  ._getIconUrl;
+leaflet.Icon.Default.mergeOptions({ iconUrl, iconRetinaUrl, shadowUrl });
 
 const APP_NAME = "Geocoin Carrier";
 const APP = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
+
+interface Cell {
+  readonly i: number;
+  readonly j: number;
+}
+
+interface Cache {
+  coins: number;
+}
+
+/*
+Interfaces, Functions, and Events for later
+interface Coin {
+  cell: Cell;
+  serial: number;
+}
+
+function collect(coin: Coin, cell: Cell) {
+    if (!cell) { return; }
+
+}
+
+function deposit(coin: Coin, cell: Cell) {
+    if (!cell) { return; }
+}
+
+const updateCache = new CustomEvent("cache-updated");
+const playerMoved = new CustomEvent("player-moved", {
+    detail: {movement}
+});
+const inventoryChanged = new CustomEvent("player-inventory-changed", {
+    detail: {item}
+})
+*/
 
 const BUTTON = document.createElement("button");
 const BUTTON_TEXT = "Click me!";
